b493036bb527f5321eba2dba05cf987eb655800c
mosheco110 <mosheco110@gmail.com>
2018-03-07 12:50:27 +0200

    Test Cases for stage 1  Point3D compareTo  Point3D toString  Point3D add  Point3D subtract  Point3D distance  Vector Add test  Vector Substruct test  Vector Scaling test  Vector Dot product test  Vector Length test  Vector Normalize test  Vector Cross product test  Camera test  Triangle intersection test  Triangle getNormal test  Sphere intersection test  Sphere getNormal test  Plane intersection test

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,2 +1,25 @@
 public class Coordinate {
+
+
+    private double _coordinate;
+
+    // ***************** Constructors ********************** //
+    public Coordinate(){this._coordinate=0;};
+
+    public Coordinate(double coordinate){this._coordinate=coordinate;};
+
+    public Coordinate(Coordinate coordinate)
+
+    // ***************** Getters/Setters ********************** //
+    public double getCoordinate();
+
+    public void setCoordinate(double coordinate)
+
+    // ***************** Administration ******************** //
+    public int compareTo(Coordinate coordinate);
+
+    // ***************** Operations ******************** //
+    public void add(Coordinate coordinate);
+
+    public void subtract(Coordinate coordinate);
 }
