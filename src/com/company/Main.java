92df9584504f28d2417053f47405ec18c86d3f46
mosheco110 <mosheco110@gmail.com>
2018-03-07 09:48:53 +0200

    Test Cases for stage 1  Point3D compareTo  Point3D toString  Point3D add  Point3D subtract  Point3D distance  Vector Add test  Vector Substruct test  Vector Scaling test  Vector Dot product test  Vector Length test  Vector Normalize test  Vector Cross product test  Camera test  Triangle intersection test  Triangle getNormal test  Sphere intersection test  Sphere getNormal test  Plane intersection test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,26 @@
 package com.company;
+/*
+* Mini- project in Software Engineering
 
+ Point3D compareTo
+ Point3D toString
+ Point3D add
+ Point3D subtract
+ Point3D distance
+ Vector Add
+ Vector Substruct
+ Vector Scaling
+ Vector Dot product
+ Vector Length
+ Vector Normalize
+ Vector Cross product
+ Camera
+ Triangle intersection
+ Triangle getNormal
+ Sphere intersection
+ Sphere getNormal
+ Plane intersection
+ */
 public class Main {
 
     public static void main(String[] args) {
